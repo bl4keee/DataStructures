b0d5bc64145910601dfc9d88ac9c42a641bf20ff
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-03-04 11:48:41 +0100

    Add files via upload

---
+++ Static/TestStudent.java
@@ -0,0 +1,15 @@
+package staticKeyword;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student(1, "Alex");
+		Student s2 = new Student(2, "Blake");
+		
+		s1.displayValues();
+		s2.displayValues();
+
+	}
+
+}
